07ba16605f8b1edf5e97709e71dd0ab37a3a3f1e
Eric Nantel <9371020+ericnantel@users.noreply.github.com>
2024-03-12 13:54:39 -0400

    Added/Updated Lexer Structs And Enum Files (#22)
    * Added lexer/public/lexer_token_class_number_sign.hpp
    * Updated lexer/public/lexer_token.hpp
    * Updated lexer/public/lexer_token_class.hpp
    * Updated lexer/public/lexer_token_class_keyword.hpp
    * Updated lexer/public/lexer_token_class_number.hpp
    * Updated lexer/public/lexer_token_class_symbol.hpp
    * Updated parser/CMakeLists.txt
    * Updated scanner/CMakeLists.txt

---
+++ toolchain/compilers/frontend/mrald-craft/lexer/public/lexer_token_class_number_sign.hpp
@@ -0,0 +1,20 @@
+
+#ifndef MRALD_CRAFT_LEXER_TOKEN_CLASS_NUMBER_SIGN_HPP
+#define MRALD_CRAFT_LEXER_TOKEN_CLASS_NUMBER_SIGN_HPP
+
+namespace Mrald
+{
+	namespace Craft
+	{
+		namespace Lexer
+		{
+			enum class TokenClassNumberSign
+			{
+				Positive = 0,
+				Negative
+			};
+		};
+	};
+};
+
+#endif
